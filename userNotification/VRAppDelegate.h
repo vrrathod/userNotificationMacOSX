ea020ee13187ad18d7d6cef9e0caeb63518d77c6
Viral Rathod <viral@Cappuccino.local>
2014-04-08 23:28:01 -0700

    basic working

--- userNotification/VRAppDelegate.h
+++ userNotification/VRAppDelegate.h
@@ -8,8 +8,28 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface VRAppDelegate : NSObject <NSApplicationDelegate>
+////////////////////////////////////////////////////////////////////////////////////////////
+@interface VRAppDelegate : NSObject <NSApplicationDelegate, NSUserNotificationCenterDelegate>
 
+////////////////////////////////////////////////////////////////////////////////////////////
 @property (assign) IBOutlet NSWindow *window;
+@property NSUserNotificationCenter* nc;
 
+////////////////////////////////////////////////////////////////////////////////////////////
+@property (weak) IBOutlet NSButton *btnHasAction;
+@property (weak) IBOutlet NSTextField *textActionTitle;
+@property (weak) IBOutlet NSTextField *labelActionTitle;
+@property (weak) IBOutlet NSTextField *textOtherTitle;
+@property (weak) IBOutlet NSTextField *title;
+@property (weak) IBOutlet NSTextField *textSubTitle;
+@property (weak) IBOutlet NSTextField *textInformation;
+@property (weak) IBOutlet NSTextField *labelUserActionInfo;
+
+////////////////////////////////////////////////////////////////////////////////////////////
+- (IBAction)hasActionButtonClicked:(id)sender;
+
+////////////////////////////////////////////////////////////////////////////////////////////
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)notification;
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center didActivateNotification:(NSUserNotification *)notification;
+////////////////////////////////////////////////////////////////////////////////////////////
 @end
--- userNotification/VRAppDelegate.m
+++ userNotification/VRAppDelegate.m
@@ -10,9 +10,65 @@
 
 @implementation VRAppDelegate
 
+@synthesize nc;
+@synthesize labelActionTitle, textActionTitle, btnHasAction;
+@synthesize textOtherTitle;
+@synthesize title, textSubTitle, textInformation;
+@synthesize labelUserActionInfo;
+
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    nc = [NSUserNotificationCenter defaultUserNotificationCenter];
+    [nc setDelegate:self];
 }
 
+- (IBAction)showNotification:(id)sender{
+    NSLog(@"%s", __FUNCTION__);
+    [self.labelUserActionInfo setStringValue:@"Label"];
+    NSUserNotification* notification = [[NSUserNotification alloc] init];
+    notification.title = self.title.stringValue;
+    notification.subtitle = self.textSubTitle.stringValue;
+    notification.informativeText = self.textInformation.stringValue;
+    notification.hasActionButton = (NSOnState == [self.btnHasAction state]);
+    notification.actionButtonTitle = self.textActionTitle.stringValue;
+    notification.otherButtonTitle = self.textOtherTitle.stringValue;
+    notification.deliveryDate = [NSDate dateWithTimeInterval:1 sinceDate:[NSDate date]];
+    [nc scheduleNotification:notification];
+}
+
+- (IBAction)hasActionButtonClicked:(id)sender {
+    BOOL showAction = (NSOnState == [sender state]);
+    [self.textActionTitle setEnabled:showAction];
+    [self.labelActionTitle setEnabled:showAction];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center
+     shouldPresentNotification:(NSUserNotification *)notification {
+    NSLog(@"%s", __FUNCTION__);
+    return YES;
+}
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center
+       didActivateNotification:(NSUserNotification *)notification {
+    NSString* message = @"";
+    switch (notification.activationType) {
+        case NSUserNotificationActivationTypeActionButtonClicked:
+            message =@"Action Button Clicked";
+            break;
+        case NSUserNotificationActivationTypeContentsClicked:
+            message = @"Contents Clicked";
+            break;
+        case NSUserNotificationActivationTypeReplied:
+            message = @"Replied ??";
+            break;
+        case NSUserNotificationActivationTypeNone:
+        default:
+            message = @"None";
+            break;
+    }
+    NSLog(@"%@", message);
+    [self.labelUserActionInfo setStringValue:message];
+}
 @end
